eab4a1df08d3e21cf3fbdc989858f8e2d277db04
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-18 20:20:54 -0500

    completed

--- 02-es6/24-createAnExportFallbackWithDefault/index.js
+++ 02-es6/24-createAnExportFallbackWithDefault/index.js
@@ -3,6 +3,6 @@
 The following function should be the fallback value for the module. Please add the necessary code to do so.
 */
 
-function subtract(x, y) {
+export default function subtract(x, y) {
   return x - y;
 }
