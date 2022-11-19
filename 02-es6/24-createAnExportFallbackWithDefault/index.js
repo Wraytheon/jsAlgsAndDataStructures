f63f2e683845675a70f8e7c7454cba84c9053cd6
Wraytheon <94151369+Wraytheon@users.noreply.github.com>
2022-11-18 20:09:54 -0500

    added basic info/files

---
+++ 02-es6/24-createAnExportFallbackWithDefault/index.js
@@ -0,0 +1,8 @@
+/* Directions */
+/*
+The following function should be the fallback value for the module. Please add the necessary code to do so.
+*/
+
+function subtract(x, y) {
+  return x - y;
+}
